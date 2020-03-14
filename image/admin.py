b9c87adb22b9ba2c4ad0c7385c73865ca665c462
Sonderman <alihaydar338@gmail.com>
2020-03-14 17:51:10 +0300

    Admin panel için superUser olusturuldu, admin:123123 ,

--- image/admin.py
+++ image/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here
+
+from .models import Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    # fields = ['title', 'status']
+    list_display = ['title', 'status']
+    list_filter = ['status']
+
+
+admin.site.register(Category, CategoryAdmin)
--- image/models.py
+++ image/models.py
@@ -12,7 +12,7 @@ class Category(models.Model):
     title = models.CharField(max_length=30)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-    image = models.ImageField(blank=True, upload_to='images/')
+    image = models.ImageField(blank=True, upload_to='images/')  # py -m pip install --upgrade Pillow
     status = models.CharField(max_length=10, choices=STATUS)
     slug = models.SlugField()
     parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
