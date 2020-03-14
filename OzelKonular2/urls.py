855c02009aacc8e84b0f4a121a6475357fe8cb28
Sonderman <alihaydar338@gmail.com>
2020-03-14 16:12:57 +0300

    veritabanı table olusturuldu ve migrate yapıldı.

--- OzelKonular2/urls.py
+++ OzelKonular2/urls.py
@@ -13,10 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
+"""
+Burası sitemizin Routing kısmının ayarlandığı yer
+"""
 urlpatterns = [
-    path('home/', include('home.urls')),
+    path('', include('home.urls')),
+    #path('home/', include('home.urls')),
+    path('image/', include('image.urls')),
     path('admin/', admin.site.urls),
 ]
