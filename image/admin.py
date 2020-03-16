38c260a59aa8cb9e4ced6c2e3f46cb1b5c609042
Sonderman <alihaydar338@gmail.com>
2020-03-16 14:13:08 +0300

    7. video bitti, admin resi galerisi eklendi.

--- image/admin.py
+++ image/admin.py
@@ -5,6 +5,11 @@ from django.contrib import admin
 from .models import *
 
 
+class ImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
+
+
 class CategoryAdmin(admin.ModelAdmin):
     # fields = ['title', 'status']
     list_display = ['title', 'status']
@@ -15,7 +20,13 @@ class ImageAdmin(admin.ModelAdmin):
     # fields = ['title', 'status']
     list_display = ['title', 'category', 'status']
     list_filter = ['category']
+    inlines = [ImageInline]
+
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'parent_image', 'image']
 
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Image, ImageAdmin)
+admin.site.register(Images, ImagesAdmin)
--- image/models.py
+++ image/models.py
@@ -41,3 +41,12 @@ class Image(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Images(models.Model):
+    parent_image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, blank=True)
+    image = models.ImageField(blank=True, upload_to='images/', max_length=255)  # py -m pip install --upgrade Pillow
+
+    def __str__(self):
+        return self.title
