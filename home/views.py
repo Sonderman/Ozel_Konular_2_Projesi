1cd8e6f0ec0d65fc0db921fd0aacde983dc7871c
Sonderman <alihaydar338@gmail.com>
2020-03-18 21:22:23 +0300

    9. video bitti. setting entity yapıldı.

--- home/views.py
+++ home/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
 # Create your views here.
-def index(request):
-    unv="Karabuk Universitesi"
-    fakulte="Muhendislik F."
-    context={'unv':unv,'fakulte':fakulte}
-    return render(request,'index.html',context)
+from .models import *
 
+
+def index(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting}
+    return render(request, 'index.html', context)
