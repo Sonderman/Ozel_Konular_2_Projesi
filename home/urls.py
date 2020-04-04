3ecf5769a6c20387f66c112310b374bb545b0ae5
Sonderman <alihaydar338@gmail.com>
2020-04-04 20:00:45 +0300

    13. video bitti. Contact form yapıldı.

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contact', views.contact, name='contact'),
     path('aboutus', views.aboutus, name='aboutus'),
+    path('references', views.references, name='references'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
-# Create your views here.
+from django.contrib import messages
 from .models import *
 
 
@@ -12,8 +11,21 @@ def index(request):
 
 
 def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()
+            data.name = form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request, "Your message has been succesfully sent, Thank you")
+            return HttpResponseRedirect('/contact')
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting}
+    form = ContactForm()
+    context = {'setting': setting, 'form': form}
     return render(request, 'Pages/contactPage.html', context)
 
 
@@ -21,3 +33,9 @@ def aboutus(request):
     setting = Setting.objects.get(pk=1)
     context = {'setting': setting}
     return render(request, 'Pages/aboutUsPage.html', context)
+
+
+def references(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting}
+    return render(request, 'Pages/referencesPage.html', context)
